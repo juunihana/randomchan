425ec21232fe1a034afc630159c91dab549d70a9
juunihana <juunigatsunohana@gmail.com>
2020-05-31 17:55:35 +0300

    Initial commit

---
+++ src/main/java/org/projects/randomchan/RandomchanApplication.java
@@ -0,0 +1,13 @@
+package org.projects.randomchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomchanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomchanApplication.class, args);
+    }
+
+}
